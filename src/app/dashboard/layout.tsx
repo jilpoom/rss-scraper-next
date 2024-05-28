c7f06d5ca8e13a5563ca3344fce14705f5541ea2
Palladium <wjddyd94@naver.com>
2024-05-29 01:19:40 +0900

    Rss-Scraper-Next Init

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+import React from "react";
+
+export default function Layout({children}: { children: React.ReactNode }) {
+    return (
+        <>
+            <Button>Click me</Button>
+        </>
+    )
+}
